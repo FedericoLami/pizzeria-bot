edf293d5922340943fcb01a03c96f1340ba51ecf
Federico <federicolami@hotmail.com>
2026-06-26 18:28:13 -0300

    feat: agregar conexion a postgres y funcion para obtener productos

---
+++ src/postgres.py
@@ -0,0 +1,20 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pgConnection = psycopg2.connect(
+    host = os.environ.get("POSTGRES_HOST"),
+    port = os.environ.get("POSTGRES_PORT"),
+    dbname = os.environ.get("POSTGRES_DB"),
+    user = os.environ.get("POSTGRES_USER"),
+    password = os.environ.get("POSTGRES_PASSWORD"),
+)
+
+pgCursor = pgConnection.cursor()
+
+def obtener_productos():
+    pgCursor.execute("SELECT nombreProducto, disponible, monto, descripcion FROM productos")
+    resultados = pgCursor.fetchall()
+    return resultados

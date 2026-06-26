d5b048a9068d6c02908cb07c7729049b81296177
Federico <federicolami@hotmail.com>
2026-06-26 11:35:27 -0300

    feat: agregar modulo de sesiones con Redis (crear id, guardar y leer historial)

---
+++ src/sesion.py
@@ -0,0 +1,30 @@
+import redis
+from dotenv import load_dotenv
+import os
+import random
+import json
+
+load_dotenv()
+
+redisClient = redis.Redis(
+    host = os.environ.get("REDIS_HOST"),
+    port = os.environ.get("REDIS_PORT"),
+    decode_responses=True
+)
+
+def create_session_id(nombre):
+    return nombre + "_" + str(random.randint(100000,999999))
+
+
+def guardar_historial(session_id,historial):
+    historial_str = json.dumps(historial)
+    redisClient.set("sesion:" + session_id, historial_str, ex = 300) 
+
+def leer_historial(session_id):
+    sesion = ("sesion:" + session_id)
+    sesionRedis = redisClient.get(sesion)
+    if sesionRedis:
+        return json.loads(sesionRedis)
+    else:
+        return []
+    

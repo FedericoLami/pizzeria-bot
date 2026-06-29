9a63c8dd988e7c0b4c8359e79e05bd4599e7a272
Federico <federicolami@hotmail.com>
2026-06-29 16:42:21 -0300

    feat: agregar datos de prueba de productos(4 pizzas)

---
+++ database/seed.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO productos(nombreProducto,monto,disponible,descripcion)
+VALUES ('Muzzarella', 5000, true, 'Pizza con salsa de tomate y abundante muzzarella');
+
+INSERT INTO productos(nombreProducto,monto,disponible,descripcion)
+VALUES ('Napolitana', 6500, true, 'Pizza con salsa de tomate, abundante muzzarella y tomates en rodajas');
+
+INSERT INTO productos(nombreProducto,monto,disponible,descripcion)
+VALUES ('Fugazzeta', 6500, true, 'Pizza con salsa de tomate, abundante muzzarella y abundante cebolla');
+
+INSERT INTO productos(nombreProducto,monto,disponible,descripcion)
+VALUES ('Especial', 7000, true, 'Pizza con salsa de tomate, abundante muzzarella, jamon cocido rallado y morrones');
